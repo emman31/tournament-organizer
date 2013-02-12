29f2454156e44a88439f159a6d7a9fb69ba40aad
Emmanuel Chartrand <chartrande@grics.qc.ca>
2013-02-12 13:28:14 -0500

    Adjust configs behaviors.

--- javascript/configs.js
+++ javascript/configs.js
@@ -1,9 +1,54 @@
 var nextParticipantId = 0;
 
 function configInit() {
+  // Retrieve participants from local storage.
+  var savedParticipants = localStorage.getItem("tournament-participants");
+  if (savedParticipants) {
+    $.each(JSON.parse(savedParticipants), function(index, value) {
+      addInput();
+      fillLastParticipant(value);
+    });
+  }
+  
+  // Retrieve team size from local storage
+  var savedTeamSize = localStorage.getItem("tournament-teamSize");
+  if (savedTeamSize) {
+    $("#team-size").val(savedTeamSize);
+  }
+  
+  // Retrieve tournament type from local storage
+  var savedTournamentType = localStorage.getItem("tournament-type");
+  if (savedTournamentType) {
+    $("#tournament-type #" + savedTournamentType).check();
+  }
+
   // Add delete behavior on the delete button.
-  $('#participants').delegate('.delete', 'click', function(){
+  $("#participants").delegate(".delete", "click", function(){
     $(this).parent().remove();
+  });
+  
+  // Save participants on each change.
+  $("#participants").delegate("input[id*='participant-']", "change", function() {
+    localStorage.setItem("tournament-participants", JSON.stringify(getAllParticipants()));
+  });
+  
+  // Save team size on change
+  $("#team-size").change(function() {
+    localStorage.setItem("tournament-teamSize", $("#team-size").val());
+  });
+  
+  /*
+  // Save tournament Type
+  $("#team-size").change(function() {
+    localStorage.setItem("tournament-type", $("#tournament-type input:checked").val());
+  });*/
+  
+  // Add behavior to create a new input on 'enter' pressed.
+  $("#participants").delegate("input[id*='participant-']", "keyup", function(e) {
+    if (e.keyCode == 13) { // Enter
+      addInput();
+      focusLastParticipant();
+    }
   });
 }
 
@@ -18,14 +63,34 @@ function addInput() {
       primary: "ui-icon-circle-close"
     },
   });
+  
+  focusLastParticipant();
 }
 
 function generateTournament() {
-	switch($('#tournament-type').val()){
-		case "single-list":
-			break;
-			
-	}
+  switch($("#tournament-type input:checked").val()){
+    case "single-list":
+      generateSingleList();
+      break;
+  }
+}
+
+function getAllParticipants() {
+  var participants = [];
+  $("#participants input[id*='participant-']").each(function() {
+    if ($(this).val() !== "") {
+      participants.push($(this).val());
+    }
+  });
+  return participants;
+}
+
+function focusLastParticipant() {
+  $("#participants input[id*='participant-']:last").focus();
+}
+
+function fillLastParticipant(name) {
+	$("#participants input[id*='participant-']:last").val(name);
 }
 
 function getInput(nb) {
--- javascript/single-list.js
+++ javascript/single-list.js
@@ -1,3 +1,11 @@
 function generateSingleList() {
-	alert("Single list tournament generated");
+  var participants = getAllParticipants();
+  $("#tournament-tree").html("");
+  $.each(participants, function(index, value) {
+    $("#tournament-tree").append(getSingleListParticipantInput(value, index));
+  });
+}
+
+function getSingleListParticipantInput(name, nb) {
+	return "<label for='single-list-participant-" + nb + "'>" + name + "</label><input type='text' id='single-list-participant-" + nb + "'/><br />";
 }
